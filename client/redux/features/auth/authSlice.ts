a3a27ca43b27402930dc508e2d8c391e9f1f9580
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2024-10-31 22:44:28 +0700

    use local storage to sync state with redux

--- client/redux/features/auth/authSlice.ts
+++ client/redux/features/auth/authSlice.ts
@@ -1,9 +1,15 @@
+// authSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+// Helper function to check if we're in the browser
+const isBrowser = typeof window !== 'undefined';
+
 const initialState = {
-    token: '',
+    token: isBrowser ? localStorage.getItem('token') || '' : '',
     user: '',
-    isLoggedIn: false
+    isLoggedIn: isBrowser
+        ? localStorage.getItem('isLoggedIn') === 'true'
+        : false
 };
 
 interface IRegistrationPayload {
@@ -24,19 +30,34 @@ const authSlice = createSlice({
             action: PayloadAction<IRegistrationPayload>
         ) => {
             state.token = action.payload.token;
+            if (isBrowser) localStorage.setItem('token', action.payload.token);
         },
-        userLoggedIn: (state, action: PayloadAction<any>) => {
-            (state.token = action.payload.accessToken),
-                (state.user = action.payload.user);
+        userLoggedIn: (state, action: PayloadAction<ILoginPayload>) => {
+            state.token = action.payload.accessToken;
+            state.user = action.payload.user;
             state.isLoggedIn = true;
+            if (isBrowser) {
+                localStorage.setItem('token', action.payload.accessToken);
+                // localStorage.setItem(
+                //     'user',
+                //     JSON.stringify(action.payload.user)
+                // );
+                localStorage.setItem('isLoggedIn', 'true');
+            }
         },
         userLoggedOut: (state) => {
-            (state.token = ''), (state.user = ''), (state.isLoggedIn = false);
+            state.token = '';
+            state.user = '';
+            state.isLoggedIn = false;
+            if (isBrowser) {
+                localStorage.removeItem('token');
+                localStorage.removeItem('user');
+                localStorage.setItem('isLoggedIn', 'false');
+            }
         }
     }
 });
 
 export const { userRegistration, userLoggedIn, userLoggedOut } =
     authSlice.actions;
-
 export default authSlice.reducer;
