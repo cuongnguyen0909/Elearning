71eeffc9c117eb169afb3d5ce79436c085612651
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2024-11-01 11:34:20 +0700

    fix bug not display circle progress

--- client/components/common/Loading.tsx
+++ client/components/common/Loading.tsx
@@ -1,28 +1,28 @@
+import { LinearProgress } from '@mui/material';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import React from 'react';
 
 type Props = {
-    isLoading: boolean;
-    preventClick?: boolean;
+    // isLoading: boolean;
 };
 
 const Loading: React.FC<Props> = (props) => {
-    const { isLoading } = props;
     return (
         <Backdrop
             sx={(theme) => ({
                 color: '#fff',
                 zIndex: theme.zIndex.drawer + 1
             })}
-            open={isLoading}
+            open={true}
             // onClick={(event: any) => {
             //     if (props.preventClick) {
             //         event.preventDefault();
             //     }
             // }}
         >
-            <CircularProgress color="inherit" />
+            <CircularProgress color="inherit" className="z-9999999999" />
+            {/* <LinearProgress className="z-9999999999" color="inherit" /> */}
         </Backdrop>
     );
 };
