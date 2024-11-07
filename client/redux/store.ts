7486e6fd091beeaa76665957543c51254a8dffd2
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2024-11-07 20:27:50 +0700

    fix bug do not automatically refresh token

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -4,13 +4,12 @@ import { apiSlice } from './features/api/apiSlice';
 import authSlice from './features/auth/authSlice';
 
 export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authSlice
-    },
-    devTools: true,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware)
+   reducer: {
+      [apiSlice.reducerPath]: apiSlice.reducer,
+      auth: authSlice
+   },
+   devTools: true,
+   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -19,23 +18,22 @@ export type RootState = ReturnType<typeof store.getState>;
 let isRefreshing = false;
 
 const initializeApp = async () => {
-    const state = store.getState();
-    if (state.auth.isLoggedIn && !isRefreshing) {
-        isRefreshing = true;
-        try {
-            await store.dispatch(
-                apiSlice.endpoints.refreshToken.initiate(
-                    {},
-                    { forceRefetch: true }
-                )
-            );
-            await store.dispatch(
-                apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
-            );
-        } finally {
-            isRefreshing = false;
-        }
-    }
+   const state = store.getState();
+   if (state.auth.isLoggedIn && !isRefreshing) {
+      isRefreshing = true;
+      try {
+         await store.dispatch(apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true }));
+         await store.dispatch(apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true }));
+      } finally {
+         isRefreshing = false;
+      }
+   }
 };
 
-initializeApp();
+//after store is created, initialize the app every 4 minutes 59 seconds to refresh token
+setTimeout(() => {
+   initializeApp();
+   setInterval(() => {
+      initializeApp();
+   }, 299000);
+}, 1000);
