9a6e413e865997e9c3733b647f9473857b72cff9
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2024-11-09 16:46:38 +0700

    api user fearture

--- client/redux/features/user/userApi.ts
+++ client/redux/features/user/userApi.ts
@@ -1,15 +1,75 @@
 import { apiSlice } from '../api/apiSlice';
 
 export const userApi = apiSlice.injectEndpoints({
-   endpoints: (builder) => ({
-      getAllUsers: builder.query({
-         query: () => ({
-            url: 'user/all',
-            method: 'GET',
-            credentials: 'include' as const
-         })
-      })
-   })
+    endpoints: (builder) => ({
+        getAllUsers: builder.query({
+            query: () => ({
+                url: 'user/all',
+                method: 'GET',
+                credentials: 'include' as const
+            })
+        }),
+        updateUserRole: builder.mutation({
+            query: (data) => {
+                const { email } = data;
+                return {
+                    url: `user/update`,
+                    method: 'PUT',
+                    body: { email },
+                    credentials: 'include' as const
+                };
+            }
+        }),
+        deleteUserAdminRole: builder.mutation({
+            query: (data) => {
+                const { email } = data;
+                return {
+                    url: `user/delete`,
+                    method: 'PUT',
+                    body: { email },
+                    credentials: 'include' as const
+                };
+            }
+        }),
+        deleteUser: builder.mutation({
+            query: (data) => {
+                const { id } = data;
+                return {
+                    url: `user/delete/${id}`,
+                    method: 'PUT',
+                    credentials: 'include' as const
+                };
+            }
+        }),
+        blockUser: builder.mutation({
+            query: (data) => {
+                const { userId } = data;
+                return {
+                    url: `user/lock`,
+                    method: 'PUT',
+                    body: { userId },
+                    credentials: 'include' as const
+                };
+            }
+        }),
+        unLockuser: builder.mutation({
+            query: (data) => {
+                const { id } = data;
+                return {
+                    url: `user/unlock/${id}`,
+                    method: 'PUT',
+                    credentials: 'include' as const
+                };
+            }
+        })
+    })
 });
 
-export const { useGetAllUsersQuery } = userApi;
+export const {
+    useGetAllUsersQuery,
+    useUpdateUserRoleMutation,
+    useDeleteUserAdminRoleMutation,
+    useDeleteUserMutation,
+    useBlockUserMutation,
+    useUnLockuserMutation
+} = userApi;
